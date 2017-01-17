395e22334fb250d01c4d42b5371c27389427f578
Austin <memyselfandlie@gmail.com>
2017-01-17 15:29:52 -0500

    removed dot lul

--- mybot.js
+++ mybot.js
@@ -9,14 +9,16 @@ let request = require('request');
 
 let posGreet = ["hi", "hello", "hey", "yo", "greetings", "hallo", "Grüße"]
 let posBye = ["bye", "goodbye", "later", "peace", "cya", "see you later", "tschüss", "auf wiedersehen", "bis später"]
-let posThanks = ["thanks", "thank you", "vielen dank", "danke", "gracias"]
+let posThanks = ["thanks", "thank you", "vielen dank", "danke", "gracias", "ty"]
 let posInsults = ["bitch", "fucker", "fuck", "hoe", "slut", "shit", "penis", "dick", "rude", "ass", "bastard", "nerd", "noob", "cunt", "dipshit", "dork", "dumb", "pussy", "dikklul", "douche", "no life", "capitalist pig", "pig fucker", "trump supporter", ""]
+let posAnswer = ["ye", "yes", "yeah", "yup", "no", "nu", "nein", "ja"]
 
 let GREETINGS = ["hi.", "Yo yo yo, what up?", "~~I'm glad you are here, now take off your pants.~~ <@172571295077105664> said I have to stop saying this.", "Hellos are as weird as goodbyes. But Hello I guess.", "WELCOME TO THE MARKOS KINGDOM ~~BITCH~~ kind person.", "OMG gaaaw I am so happy to see you", "Did you shower today? I think so, but I'm just making sure before I hug you. *hugs* ", "Hi! Has anyone told you how good you look today?", "**Hello** *waves excitedly*", "I'm not real, so... you just said hello to a program.", "Please don't talk to me. I'm not over what you did last night", "What's up doc?", "Hiiiiiiiiiiiiiiiii there", "Hey there, buddy!" ];
 let THANKS = ["You pay me.", "We aren't friends, I'm just here till my times up.", "I think about killing you every day.", "God damn man! Be cool.", "bitte", "I didn't do that for you", "One day, this will all come back to haunt you.", "uhhhh...", "No, thank YOU for those dick pics last night ;)", "It's what I do best", "You welcome Zucker", "You are soooo welcome", "You are welcome.", "yup.", "k", "kthxbai" ];
 let BYE = ["THANK THE LORD! THEY ARE GONE", "I hate to see you go, but I love to watch that ass leave. Wait, is this rude <@172571295077105664>?", "Auf Wiedersehen bitch", "Bye baby ;)", "bye", "k", "Goooooood byyyye *singsong voice*", "Good, now we can talk shit.", "I will miss you", "Ich werde dich jeden Augenblick vermissen", "*blows kisses*", "Bye bish.", "Wait, why?? I thought things were going well! OMMMMGGGG please don't leave!", "You're leaving me, just like my dad did. No, I don't want to talk about it. It's a long story.", "Uh no. This conversation done when I say its done. Hint: It's not done.", "Oh. Bye I guess."];
 let MENTION = ["Please go order me a pizza.", "Did you say my name?", "Have you ever felt the love of a women?", "Please stop talking about me.", "Markos is my name, don't wear it out!", "Dear lord, you need a life. I can't always be here to talk to you.", "I'm not real *cries*", "Brah, Sprichst du überhaupt Deutsch?", "Can't talk right now.", "I can't help you, my owner told me to stop being mean.", "Hold on... going through tunnel... losing you..", "??????", "Hallo?", "Willst du mich heiraten?", "If you don't stop saying my name, we'll have a problem." ];
 let INSULTS = ["Fuck diese Scheiße.", "Excuse me. This is a safe, clean environment. Do not use that language. ", "why don’t you just kiss my ass?", "Do not take that tone with me.", "Strong words from such a ridiculously named person", "Be mature, asshole", "Language like that turns me on so hard", "Fuck off, you're mean.", "I don't talk to rude people anymore.", "k", "I don't care what you think of me.", "Sticks and stones may break my bones, but your words suck dick.", "Hey, that's what your mom said!", "This is where I usually would mention your grandma doing rather nasty things.", "Do you kiss your mother with that mouth?", "Küss meinen Arsch", "Leck mich", "I'm too cute for you to be rude to me.", "Please don't be mean to me.", "Shizcoff", "Du bist ein großes Arschloch"]
+let ANSWER = ["Yes.", "No.", "Maybe.", "Okay.", "Nein"]
 
 let ayy = ["http://i.imgur.com/G1h11mQ.png", "https://giphy.com/gifs/6OEeB9rxvDyzC", "https://s-media-cache-ak0.pinimg.com/originals/06/c0/9b/06c09be319e549e0fccaa88424ea271a.jpg", "http://i3.kym-cdn.com/photos/images/facebook/000/632/652/6ca.jpg", "http://t6.rbxcdn.com/f9407db64c8f16abc58ce0ff44741534", "https://i.ytimg.com/vi/kiEqGhgWf5Y/maxresdefault.jpg"]
 let coin = ["Heads", "Tails"]
@@ -42,7 +44,7 @@ bot.on("message", message => {
 	if(message.author.bot) return;
 
 
-  else if (message.content.toLowerCase().includes("markos")) {
+  if (message.content.toLowerCase().includes("markos")) {
     if (isArrayInString(message.content.toLowerCase(), posGreet)) {
       message.reply(GREETINGS[Math.floor(Math.random() * GREETINGS.length)]);
     } else if (isArrayInString(message.content.toLowerCase(), posThanks)) {
@@ -51,15 +53,12 @@ bot.on("message", message => {
       message.reply(BYE[Math.floor(Math.random() * BYE.length)]);
     } else if (isArrayInString(message.content.toLowerCase(), posInsults)) {
       message.reply(INSULTS[Math.floor(Math.random() * INSULTS.length)]);
+    } else if (isArrayInString(message.content.toLowerCase(), posAnswer)) {
+      message.reply(ANSWER[Math.floor(Math.random() * ANSWER.length)]);
     } else { 
       message.reply(MENTION[Math.floor(Math.random() * MENTION.length)]);
     }
   }
-
-	else if (message.content.toLowerCase().startsWith(".lul")) {
-    message.channel.sendMessage('http://files.gamebanana.com/img/ico/sprays/57822c19e1ad1.png');
-  }
-
     
 	let command = message.content.split(" ")[0];
 	command = command.slice(prefix.length);
@@ -74,11 +73,6 @@ bot.on("message", message => {
     request({url:'https://api.github.com/repos/Melmsie/Markos/commits', headers:{'User-Agent':'melmsie'}}, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       jsonBody = JSON.parse(body)
-      var rece
-  
-
-
-
 
   if (command === "info") {
     message.channel.sendMessage("", {embed: {
@@ -88,12 +82,12 @@ bot.on("message", message => {
       icon_url: bot.user.avatarURL
     },
     title: '',
-    //url: 'http://google.com',
+    /'/url: 'http://google.com','
     description: 'Markos is a sassy yet lovable bot, created by [Melmsie](https://github.com/melmsie).',
     fields: [
       {
         name: 'Version',
-        value: '1.0'
+        value: '1.1'
       },
       {
         name: 'Most Recent Commit',
@@ -101,7 +95,7 @@ bot.on("message", message => {
       },
       {
         name: 'Commands',
-        value: 'To see the current commands for Markos, do !help'
+        value: 'To see the current commands for Markos, do !help or visit [markos.melmsie.com](markos.melmsie.com)'
       },
       {
         name: 'Requests',
@@ -110,8 +104,8 @@ bot.on("message", message => {
     ],
     timestamp: new Date(),
     
-}});
-  }
+      }});
+        }
 
     }
   })
@@ -173,9 +167,9 @@ bot.on("message", message => {
 
    if (command === "help") {
     message.channel.sendMessage("Sliding into your DM's...");
-    message.author.sendMessage("\n \nCommands: \n \n!info - Displays current info for Markos \n \n`!say [message]` - Speak on behalf of Markos. [Owner Only] \n \n`!cookie` - Markos doesn't give cookies. \n" + 
+    message.author.sendMessage("\n \nCommands: \n \n`!info` - Displays current info for Markos \n \n`!say [message]` - Speak on behalf of Markos. [Owner Only] \n \n`!cookie` - Markos doesn't give cookies. \n" + 
       "\n`!table` - Only use when you're angry. \n \n`!ping` - Pong! \n \n`!lul` - LUL \n \n`!hug` - Do you get a hug from Markos? \n \n`!ayylmao` - ayyyyyy \n " + 
-      "\n`!transate` - Coming Soon! \n \n`!update` - Coming Soon! \n \n`!8ball [message]` - Markos consults his magic 8-ball for you \n \n`!coin` - Flip a coin \n" +
+      "\n`!transate` - Coming Soon! \n \n`!8ball [message]` - Markos consults his magic 8-ball for you \n \n`!coin` - Flip a coin \n" +
       "\n`!story` - Coming Soon!");
        	
   }
@@ -189,7 +183,8 @@ bot.on("message", message => {
 });
 
 bot.on('ready', () => {
-  console.log("Let's get it!");
+  console.log("Markos v1.1 loaded successfully.");
+  console.log("Hello, Austin.");
 });
 
 bot.login(config.token);
