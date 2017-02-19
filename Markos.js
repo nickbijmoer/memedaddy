76269791aea3b239451a68251f3d0bb0ec9a01ee
Austin <memyselfandlie@gmail.com>
2017-02-19 12:22:15 -0500

    too many changes to list. biggest is prefix change to pls

--- Markos.js
+++ Markos.js
@@ -15,18 +15,23 @@ clever.setNick("Markos")
 const stories = require("./stories.json");
 const extras = require("./arrays.json");
 
-let version = " v2.3 "
+let version = "v2.4"
 let ayy = ["http://i.imgur.com/G1h11mQ.png", "https://giphy.com/gifs/6OEeB9rxvDyzC", "https://s-media-cache-ak0.pinimg.com/originals/06/c0/9b/06c09be319e549e0fccaa88424ea271a.jpg", "http://i3.kym-cdn.com/photos/images/facebook/000/632/652/6ca.jpg", "http://t6.rbxcdn.com/f9407db64c8f16abc58ce0ff44741534", "https://i.ytimg.com/vi/kiEqGhgWf5Y/maxresdefault.jpg"];
 
 
 client.login(config.token)
 
+client.on("guildCreate", guild => {
+        guild.owner.user.dmChannel.sendMessage(" tesst ")
+    })
+
 client.on("message", msg => {
-    console.log(msg.content);
+    if (!msg.content.startsWith(config.prefix)) return false;
     if (msg.author.bot) return false;
+    console.log(msg.content);
 
     let command = msg.content.substring(config.prefix.length).toLowerCase().split(" ")[0]
-    let args = msg.content.split(" ").slice(1)
+    let args = msg.content.split(" ").slice(2)
 
 
     if (msg.content.startsWith(client.user.toString())) {
@@ -65,18 +70,17 @@ client.on("message", msg => {
 
 
         const embed = new Discord.RichEmbed()
-            .setTitle('Markos Support Server')
+            .setTitle('Markos/Nitro Support Server')
             .setAuthor('MemeDaddy Markos')
             .setColor("#3676b3")
             .setDescription(version)
             .setFooter('Need to see my commands? Do !help')
             .setURL('https://discord.gg/3GNMJBG')
 
-            .addField('Memory Usage', `${((process.memoryUsage().heapUsed / 1024) / 1024).toFixed(2)} / 1024 MB`, true)
+            .addField('Memory Usage', `${((process.memoryUsage().heapUsed / 1024) / 1024).toFixed(2)} MB`, true)
             .addField('Ping', `${(client.ping).toFixed(0)} ms`, true)
             .addField('Uptime', `${timeCon(process.uptime())}`, true)
             .addField('Guilds', client.guilds.size, true)
-            .addField('Recenty Joined Guild', client.guilds.last().name, true)
             
 
 
@@ -88,11 +92,20 @@ client.on("message", msg => {
     }
 
     if (command === "server") {
-        msg.channel.sendMessage("Oh, need to talk to Melmsie? Check yo DM's boi");
+        msg.channel.sendMessage("Oh, need to talk to Melmsie? Ｃｈｅｃｋ　ｙｏ　ＤＭ＇ｓ　ｂｏｉ");
         msg.author.sendMessage("Here's an invite to the Markos support server: https://discord.gg/3GNMJBG");
     }
 
-    if (command === "joinedservers") {
+    if (command === "vaporwave") {
+        let text = args.join(" ").replace(/a/gi, "ａ").replace(/b/gi, "ｂ").replace(/c/gi, "ｃ").replace(/d/gi, "ｄ").replace(/e/gi, "ｅ").replace(/f/gi, "ｆ").replace(/g/gi, "ｇ").replace(/h/gi, "ｈ").replace(/i/gi, "ｉ").replace(/j/gi, "ｊ").replace(/k/gi, "ｋ").replace(/l/gi, "ｌ").replace(/m/gi, "ｍ").replace(/n/gi, "ｎ").replace(/o/gi, "ｏ").replace(/p/gi, "ｐ").replace(/q/gi, "ｑ").replace(/r/gi, "ｒ").replace(/s/gi, "ｓ").replace(/t/gi, "ｔ").replace(/u/gi, "ｕ").replace(/v/gi, "ｖ").replace(/w/gi, "ｗ").replace(/x/gi, "ｘ").replace(/y/gi, "ｙ").replace(/z/gi, "ｚ")
+        msg.reply("**" + text + "**")
+    }
+    if (command === "superscript") {
+        let text = args.join(" ").replace(/a/gi, "ᵃ").replace(/b/gi, "ᵇ").replace(/c/gi, "ᶜ").replace(/d/gi, "ᵈ").replace(/e/gi, "ᵉ").replace(/f/gi, "ᶠ").replace(/g/gi, "ᵍ").replace(/h/gi, "ʰ").replace(/i/gi, "ᶦ").replace(/j/gi, "ʲ").replace(/k/gi, "ᵏ").replace(/l/gi, "ˡ").replace(/m/gi, "ᵐ").replace(/n/gi, "ⁿ").replace(/o/gi, "ᵒ").replace(/p/gi, "ᵖ").replace(/q/gi, "ᑫ").replace(/r/gi, "ʳ").replace(/s/gi, "ˢ").replace(/t/gi, "ᵗ").replace(/u/gi, "ᵘ").replace(/v/gi, "ᵛ").replace(/w/gi, "ʷ").replace(/x/gi, "ˣ").replace(/y/gi, "ʸ").replace(/z/gi, "ᶻ")
+        msg.reply("**" + text + "**")
+    }
+
+    if (command === "servers") {
         if (msg.author.id === config.owner) {
             msg.channel.sendMessage(client.guilds.array())
         }
@@ -108,7 +121,7 @@ client.on("message", msg => {
 
     if (command === "invite") {
         msg.channel.sendMessage("Ok I'll DM you a link, bb");
-        msg.author.sendMessage("Use this link to invite me to your server, or I'll meme you to death :wink: \nhttps://discordapp.com/oauth2/authorize?client_id=270904126974590976&scope=bot&permissions=67628096");
+        msg.author.sendMessage("Use this link to invite me to your server, or I'll ｍｅｍｅ　ｙｏｕ　ｔｏ　ｄｅａｔｈ :wink: \nhttps://discordapp.com/oauth2/authorize?client_id=270904126974590976&scope=bot&permissions=67628096");
     }
 
     if (command === "lul") {
@@ -151,8 +164,8 @@ client.on("message", msg => {
 
     if (command === "help" || command == "commands" || command === "command") {
         msg.channel.sendMessage("Sliding into your DM's...");
-        msg.author.sendMessage("\n \nCommands: \n \n`!1337 [message]` - 1'Ll 3nc0d3 Y0uR Me5s@g3 1Nt0 l337sp3@K! \n \n`!triggered [tag user]` - Ｔｒｉｇｇｅｒｅｄ \n \n`!copypasta` - Returns a random ｃｏｐｙｐａｓｔａ \n \n`!game [input]` - Will set Markos' Game status [bot owner only] \n \n`!gif [search term(s)]` - Will search for a gif \n \n`!xd` - XD \n \n`!info` - Displays current info for Markos \n \n`!say [message]` - Speak on behalf of Markos. [guild owner only] \n \n`!cookie` - Markos doesn't give cookies. \n" +
-            "\n`!table` - Only use when you're angry. \n \n`!ping` - Pong! \n \n`!lul` - LUL \n \n`!ayylmao` - ayyyyyy \n ");
+        msg.author.sendMessage("\n \n**Commands:** \n \n**1337 [message]** - *1'Ll 3nc0d3 Y0uR Me5s@g3 1Nt0 l337sp3@K!*\n \n**triggered [tag user]** - Ｔｒｉｇｇｅｒｅｄ\n \n**copypasta** - *Returns a random ｃｏｐｙｐａｓｔａ*\n \n**game [input]** - *Will set Markos' Game status [bot owner only]*\n \n**gif [search term(s)]** - *Will search for a gif*\n \n**xd** - *XD*\n \n**info** - *Displays current info for Markos*\n \n**say [message]** - *Speak on behalf of Markos. [guild owner only]*\n" +
+            "\n**ping** - *Pong!*\n \n**lul** - *LUL*\n \n**ayylmao** - *ayyyyyy*\n \n**server** - *Oh, need to talk to Melmsie? Do this one.*\n \n**kappa** - *Kappa. Duh.*\n \n**vaporwave** - ｖａｐｏｒｗａｖｅ ｙｏｕｒ ｔｅｘｔ ｕｐ\n \n**superscript** - ᵐᵃᵏᵉ ʸᵒᵘʳ ᵗᵉˣᵗ ᵗᵉᵉⁿʸ");
     }
 
     if (command === "say") {
@@ -168,7 +181,7 @@ client.on("message", msg => {
     }
 
     if (command === "gif") {
-        msg.channel.sendMessage("Searching for your gif...").then(mesg => {
+        msg.channel.sendMessage("Ｓｅａｒｃｈｉｎｇ　ｆｏｒ　ｙｏｕｒ　ｇｉｆ．．．").then(mesg => {
             request({
                 url: 'http://api.giphy.com/v1/gifs/search?q=' + encodeURIComponent(args.join(" ")) + '&api_key=' + config.giphy + '&limit=25'
             }, function (err, res, body) {
@@ -218,8 +231,8 @@ client.on("message", msg => {
 })
 
 client.on("ready", () => {
-    console.log("Markos" + version + "loaded successfully.");
-    client.user.setGame('!help ||' + version);
+    console.log("Markos " + version + " loaded successfully.");
+    client.user.setGame('pls help || ' + version);
     clever.create(function (err, session) {
         if (err) return console.log("Error creating cleverbot session")
         console.log("Cleverbot session created")
