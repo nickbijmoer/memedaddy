0d6d761d473b76e31c316677af8920627ad6f06b
Austin <memyselfandlie@gmail.com>
2017-02-23 04:52:07 -0500

    I don't know why. Just move on.

--- Markos.js
+++ Markos.js
@@ -3,27 +3,31 @@ const settings = require('./settings.json');
 const Discord = require("discord.js")
 const client = new Discord.Client();
 
+const cleverbot = require("cleverbot.io")
+clever = new cleverbot(config.cleverbot.token, config.cleverbot.key)
+clever.setNick("Markos")
+
 let version = "v3"
 
 client.login(config.token)
 
 client.on("message", msg => {
     if (msg.author.bot) return false;
+    if (!msg.content.startsWith(config.prefix)) return false;
     console.log(msg.content);
 
     let command = msg.content.substring(config.prefix.length).toLowerCase().split(" ")[0]
     let args = msg.content.split(" ").slice(2)
-    
-    if (!msg.content.startsWith(config.prefix)) return false;
+
 
 })
 
 client.on("ready", () => {
-    console.log("Markos " + version + " loaded successfully. Dank.");
-    client.user.setGame('pls help || ' + version);
+    console.log("Markos " + version + " loaded successfully. 👌");
+    client.user.setGame('pls help 👌');
     clever.create(function (err, session) {
         if (err) return console.log("Error creating cleverbot session")
-        console.log("Cleverbot session created")
+        console.log("Cleverbot session created. 👌")
     });
-    console.log("Welcome, Austin.");
+    console.log("Welcome, Austin. 👀");
 })
--- README.md
+++ README.md
@@ -7,7 +7,13 @@ A sassy meme bot, made for discord.
 ## Features in Progress:
 
 Memes you didn't even ask for
+
 Voice chat memes
+
 Reaction memes
 
+
+
+
+👌👀👌👀👌👀👌👀👌👀 good shit go౦ԁ sHit👌 thats ✔ some good👌👌shit right👌👌th 👌 ere👌👌👌 right✔there ✔✔if i do ƽaү so my selｆ 💯 i say so 💯 thats what im talking about right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ💯 👌👌 👌НO0ОଠＯOOＯOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ👌 👌👌 👌 💯 👌 👀 👀 👀 👌👌Good shit
 
