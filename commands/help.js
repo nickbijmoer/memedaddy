22a9a4fb541028b53820da5ab62b701cab7e7cd5
Austin Melms <memyselfandlie@gmail.com>
2017-03-13 11:14:57 -0400

    dfd

--- commands/help.js
+++ commands/help.js
@@ -1,64 +1,62 @@
 exports.run = function (client, msg, args, config, Discord) {
 
-   msg.author.sendCode("adoc",
-       
-       "\nFun\n" +
-       "========\n" +
-       "9gag - get a random trending image from 9GAG" +
-       "asktrump <question to ask president trump> - trump will give his opinion on your question" +
-       "feelsbadman - returns a pepe\n" +
-       "glorify <mention user> - replaces 'Chuck Norris' with the mentioned user in a Norris joke\n" +
-       "insultme - insults you\n" +
-       "joke - Sends a random one-liner (can be nsfw)\n" +
-       "kitty - Meow, a random kitten!\n" +
-       "knock - who's there?\n" +
-       "leet <message> - translates your message to 1337 speak\n" +
-       "lenny - lenny?\n" +
-       "lmgtfy <google search> - when someone is too dumb to google something, use this for educational purposes\n" +
-       "lul - lulface\n" +
-       "mama - yo mama hates this command\n" +
-       "meirl - returns a random post from /r/me_irl\n" +
-       "norris - returns a random chuck norris joke\n" +
-       "pun - gotta love puns, right?\n" +
-       "pupper - bark bark!\n" +
-       "rip <TYPE user's name> - I can't explain this one. Do NOT mention someone.\n" +
-       "shitpost - warning: shitpost\n" +
-       "superscript <message> - translates your message to superscript\n" +
-       "trigger <mention user> - will trigger someone" +
-       "urban <search term> - will return an urban dictionary definition" +
-       "vaporwave <message> - translates the message into vaporwave\n" +
-       "xkcd <query> - fetches an XKCD comic. Leave no query for a random comic.\n" +
-       "zalgo <message> - z̻͙͎̘̻̹̰͟a̭͎̥l̬̗͟g̛̠͎̹̗̠̮ͅo̯̭̣͎̺̰̠\n" +
-       
-       "\nInfo/Tools\n" +
-       "========\n" +
-       "ban <user> <reason> - Swing that hammer\n" +
-       "bug <bug report|feature request> - requests a feature or reports a bug to the owner\n" +
-       "help - ...?\n" +
-       "info - returns some info about Markos and Melmsie\n" +
-       "kick <user> <reason> - kick his ass!\n" +
-       "ping - pong!\n" +
-       "stats - returns basic stats about Markos\n" +
-       "support - an invite to Markos' support server\n" +
-       "uptime - returns the uptime of Markos\n" +
+    msg.author.sendCode("adoc",
 
-       "\nMelmsie Only\n" +
-       "========\n" +
-       "ban <user|guild> - bans a user|guild from using Markos\n" +
-       "eval <js expression> - runs some js\n" +
-       "reboot - reboots Markos\n" +
-       "say - make Markos say dumb stuff\n" +
+        "\nFun\n" +
+        "========\n" +
+        "9gag - get a random trending image from 9GAG\n" +
+        "asktrump <question to ask president trump> - trump will give his opinion on your question\n" +
+        "feelsbadman - returns a pepe\n" +
+        "flip <message> - flips your message upside down\n" +
+        "glorify <mention user> - replaces 'Chuck Norris' with the mentioned user in a Norris joke\n" +
+        "insultme - insults you\n" +
+        "joke - Sends a random one-liner (can be nsfw)\n" +
+        "kitty - Meow, a random kitten!\n" +
+        "knock - who's there?\n" +
+        "leet <message> - translates your message to 1337 speak\n" +
+        "lenny - lenny?\n" +
+        "lmgtfy <google search> - when someone is too dumb to google something, use this for educational purposes\n" +
+        "lul - lulface\n" +
+        "mama - yo mama hates this command\n" +
+        "meirl - returns a random post from /r/me_irl\n" +
+        "norris - returns a random chuck norris joke\n" +
+        "pun - gotta love puns, right?\n" +
+        "pupper - bark bark!\n" +
+        "rip <TYPE user's name> - I can't explain this one. Do NOT mention someone.\n" +
+        "shitpost - warning: shitpost\n" +
+        "superscript <message> - translates your message to superscript\n" +
+        "trigger <mention user> - will trigger someone" +
+        "urban <search term> - will return an urban dictionary definition" +
+        "vaporwave <message> - translates the message into vaporwave\n" +
+        "xkcd <query> - fetches an XKCD comic. Leave no query for a random comic.\n" +
+        "zalgo <message> - z̻͙͎̘̻̹̰͟a̭͎̥l̬̗͟g̛̠͎̹̗̠̮ͅo̯̭̣͎̺̰̠\n" +
 
-       "\nComing Soon\n" +
-       "========\n" +
-       "ram - detailed report about RAM usage\n" +
-       "a game?\n" +
-       "meme generator - ez ez ez\n" 
+        "\nInfo/Tools\n" +
+        "========\n" +
+        "ban <user> <reason> - Swing that hammer\n" +
+        "bug <bug report|feature request> - requests a feature or reports a bug to the owner\n" +
+        "help - ...?\n" +
+        "info - returns some info about Markos and Melmsie\n" +
+        "kick <user> <reason> - kick his ass!\n" +
+        "ping - pong!\n" +
+        "stats - returns basic stats about Markos\n" +
+        "support - an invite to Markos' support server\n" +
+        "uptime - returns the uptime of Markos\n" +
 
-       
-   )
+        "\nMelmsie Only\n" +
+        "========\n" +
+        "ban <user|guild> - bans a user|guild from using Markos\n" +
+        "eval <js expression> - runs some js\n" +
+        "reboot - reboots Markos\n" +
+        "say - make Markos say dumb stuff\n" +
+
+        "\nComing Soon\n" +
+        "========\n" +
+        "ram - detailed report about RAM usage\n" +
+        "a game?\n" +
+        "meme generator - ez ez ez\n"
+
+
+    )
 
 }
-
-
-    
