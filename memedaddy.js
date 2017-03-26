d214cdd5f7cd2ec7d6183f580450e47f9b016679
Austin Melms <memyselfandlie@gmail.com>
2017-03-26 12:58:36 -0400

    I'm so sick of forgeting to commit things lol

--- memedaddy.js
+++ memedaddy.js
@@ -12,10 +12,8 @@ client.on('message', msg => {
   if (msg.author.bot || !msg.content.startsWith(config.prefix)) {
     return
   }
-
   const command = msg.content.substring(config.prefix.length).toLowerCase().split(' ')[0]
   const args = msg.content.split(' ').slice(2)
-
   if (command === 'eval') {
     const {
       username,
@@ -24,30 +22,24 @@ client.on('message', msg => {
     } = msg.author
     const owner = client.users.get(config.owner)
     const script = args.join(' ')
-
     if (msg.author.id === config.owner) {
       try {
         const dank = eval(script) // eslint-disable-line no-eval
-
         msg.channel.sendCode('js', dank)
       } catch (e) {
         msg.channel.sendMessage(':warning: **ERROR** :warning: ```\n' + e + '\n```')
       }
     } else {
       msg.reply('you don\'t have permission to use this command.')
-
       owner.sendMessage(`**${username}#${discriminator} (${id}):**\nThis person was trying to use the eval command!\n${script}`)
     }
-
   } else {
     fs.access(path.join(commandsPath, command + '.js'), fs.constants.R_OK, (err) => {
       if (err) {
         return
       }
-
       try {
         delete require.cache[require.resolve('./commands/' + command)]
-
         require('./commands/' + command).run(client, msg, args, config, Discord)
       } catch (err) {
         console.error(err)
@@ -57,7 +49,7 @@ client.on('message', msg => {
 })
 
 client.on('guildCreate', guild => {
-client.user.setGame('in ' + client.guilds.size + ' guilds')
+  client.user.setGame('in ' + client.guilds.size + ' guilds')
   client.guilds.get(guild.id).fetchMembers()
     .then(x => {
       let c = (x.members.filter(guildMember => guildMember.user.bot).array().length);
@@ -80,12 +72,10 @@ client.user.setGame('in ' + client.guilds.size + ' guilds')
 })
 
 client.on('guildDelete', guild => {
-
+  client.user.setGame('in ' + client.guilds.size + ' guilds')
   const embed = new Discord.RichEmbed()
-
     .setAuthor('MemeDaddy has left ' + guild.name)
     .setColor('#ff6161')
-
   client.guilds.get('281482896265707520').channels.get('287398833468604416').sendEmbed(embed, {
     disableEveryone: true
   })
@@ -95,7 +85,6 @@ client.on('guildDelete', guild => {
 client.on('ready', () => {
   console.log('MemeDaddy ' + config.version + ' loaded successfully. 👌')
   client.user.setGame('in ' + client.guilds.size + ' guilds')
-  console.log('Welcome, Austin. 👀')
 })
 
 process.on('unhandledRejection', err => {
