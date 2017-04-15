d0edc9d0413bcaf62ec893ce996add095e8c74de
Austin Melms <memyselfandlie@gmail.com>
2017-04-14 20:27:00 -0400

    bash me pls

--- commands/bash.js
+++ commands/bash.js
@@ -1,17 +1,17 @@
 exports.run = function (client, msg, args, config, Discord) {
     const exec = require("child_process").exec;
-    const args = msg.content.split(' ').slice(1).join(" ")
+    const argz = msg.content.split(' ').slice(1).join(" ")
     msg.delete()
     if (msg.author.id === config.owner) {
         try {
-            msg.channel.sendMessage(`**Input**\n\`\`\`bash\n$ ${args}\n\`\`\``)
-            exec(`${args}`, (stderr, stdout) => {
+            msg.channel.sendMessage(`**Input**\n\`\`\`bash\n$ ${argz}\n\`\`\``)
+            exec(`${argz}`, (stderr, stdout) => {
                 if (stdout) msg.channel.sendMessage(`**Output**\n\`\`\`bash\n${stdout}\n\`\`\``);
                 if (stderr) msg.channel.sendMessage(`**Errors**\n\`\`\`bash\n${stderr}\n\`\`\``);
             })
         } catch (e) {
             msg.channel.sendMessage(`**Error!**\n\`\`\`js\n${e}\n\`\`\``)
-            console.log(e)
+            console.log(date() + e)
         }
     } else {
         msg.reply('... how bout no?')
